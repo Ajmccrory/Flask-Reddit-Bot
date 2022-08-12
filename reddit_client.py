a5a6c96010b8c15f475532aa1305382e29d19443
AJ McCrory <93270610+Ajmccrory@users.noreply.github.com>
2022-08-12 00:18:51 -0400

    Update reddit_client.py

--- reddit_client.py
+++ reddit_client.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import json
 import time
 
@@ -7,7 +8,10 @@ import praw
 
 from prawcore.exceptions import OAuthException, Forbidden
 
+sub_id = ""
+
 class RedditClient:
+    start = time.time()
     user = None
     keep_alive = True
     prog_bar = time.time()
@@ -29,8 +33,10 @@ class RedditClient:
             OAuthException:     If authentication fails using OAuth2.
             Forbidden:          If username is None.
             KeyError:           If dictionary keys are not found.
+
         '''
-            self.client_id = self.client_secret = self.username = self.password = self.user_agent = None
+            self.user_agent = 'Autovote2 by /u/ajmccrory1'
+            self.client_id = self.client_secret = self.username = self.password = None
 
             with open('credentials.json') as credentials:
 
@@ -41,55 +47,53 @@ class RedditClient:
                 self.password = data["password"]
                 try:
 
-                    self.user = praw.Reddit (
-                                            user_agent=self.user_agent,
+                    reddit = praw.Reddit (
                                             client_id=self.client_id,
                                             client_secret=self.client_secret,
                                             password=self.password,
-                                            username=self.username
+                                            user_agent=self.user_agent,
+                                            username=self.username,
                         )
+       
 
-                    if self.user.user.me() == self.username:
-                        print('---> Logged into Reddit as {0}.'.format(self.user.user.me()))
-                        vote_request = vote_request()
-                        return vote_request
+                    if reddit.user.me() == self.username:
+                        print('---> Logged into Reddit as {0}.'.format(reddit.user.me()))
                     else:
                         raise OAuthException
                 except(OAuthException, Forbidden) as e:
                     print('---> Failed login due to {0}: invalid credentials.'.format(type(e).__name__))
                     sys.exit()
 
-    def prompt_user(start_time):
-            if int(start_time) <= 900:
-                return app.actions_performed()
+    def prompt_user(self, user_input):
+            return user_input in self.valid
 
 
     def vote_request(self):       
         with open('credentials.json') as request:
             data = json.load(request)
-            Number = int(data["Number"])
-            redditor = data["redditor"]
+            comment_id = data['comment_id']
             try:
-                upvote_target = redditor
-                Number = int(Number)
-                for comment in self.prog_bar(self.user.redditor(upvote_target).comments.new(
-                    limit=Number)):
-                    comment.upvote()
+                comment_id = comment_id
+                comment = self.comment(comment_id)
+                comment.upvote()
             except:
-                    print('Failed to upvote user: {0}. Check your inputs and try again.'.format(upvote_target))
+                print('Failed to upvote user: {0}. Check your inputs and try again.'.format(comment_id))
     
 def main():
-    start_time = time.time()
     client = RedditClient()
     while client.keep_alive:
         try:
             client.vote_request()
         except ValueError:
             pass
-        client.keep_alive = client.prompt_user(start_time)
+        client.keep_alive = client.prompt_user(input(
+            'would you like to perform another actions[y/n]\n?'
+        ))
+    print('Goodbye!')
+
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        sys.exit()
+        app.actions_performed()
